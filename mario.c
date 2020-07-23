850ca35d47665216093c387071d04f9d7dc9c280
Shyam Salil <shyamsalil1993@gmail.com>
2020-07-23 05:53:38 +0530

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,25 +1,31 @@
 #include<stdio.h>
-void main()
+#include<cs50.h>
+
+/*Program to print mario's blocks in the form of hashes with spaces to form a right aligned pyramid and a left aligned pyramid*/
+int main(void)
 {
-    int height, counter;
+    int height; /* Declaring an integer variable called height to hold the user's input as height */
+    /* We initialize a do-while loop to capture user's input while it is greater than 1 and less than 8 inclusive of 8 */
     do
     {
-        printf("Enter the height: \n");
-        scanf("%i",&height);
-    } while (height <0 || height >8);
-
-    for (int x=1 ; x<=height ; x++)
+        height = get_int("Height: ");
+    } 
+    while (height < 1 || height > 8);
+    /* We create a nested for loop that will iterate over two integer variables x and y where y will be used to print the spaces of pyramid
+     using decremental height and the last character will be an incremental # thus contributing to a right aligned pyramid */
+    for (int x = 1 ; x <= height ; x++)
     {
-        for (int y=height-x ; y!=0 ; y--)
+        for (int y = height - x ; y != 0 ; y--)
         {
             printf(" ");
         }
-        for (int z=1; z<=x ; z++)
+        for (int z = 1; z <= x ; z++)
         {
             printf("#");
         }
         printf("  ");
-        for (int w=1; w<=x ; w++)
+        /* We append a space after the right pyramid and then print out a left aligned pyramid using another variable w */
+        for (int w = 1; w <= x ; w++)
         {
             printf("#");
         }
